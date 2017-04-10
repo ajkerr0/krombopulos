2b4fa5941aab66f9fa5c70292743f2367655a454
Alex Kerr <ajkerr0@gmail.com>
2017-04-10 00:06:57 -0500

    Add raw code on gradient descent minimization and neural networks

---
+++ krombopulos/descent.py
@@ -0,0 +1,279 @@
+
+
+import sys
+from math import copysign
+
+import numpy as np
+
+EP = sys.float_info.epsilon
+SQRTEP = EP**.5
+GR = 1.618
+                                  
+def minimize(mol, n=2500, descent="cg", search="backtrack", numgrad=False,
+             eprec=1e-2, fprec=1e-2,
+             efreq=1000, nbnfreq=15):
+    """Minimize the energy of the inputted molecule.
+    
+    Args:
+        mol (Molecule): Molecule to be energy-minimized.
+    Keywords:
+        n (int): Max number of iterations in minimization run(s).
+        descent (str): Method in which the local energy minimum will be approached;
+            must be key in descentDict
+        search (str): Line search method; must be key in searchDict
+        numgrad (bool): True if the gradients are to be calculated numerically;
+            default is False
+        eprec (float): Precision for energy changes when an iteration is made 
+            signalling convergence has occurred
+        fprec (float): Precision for force magnitude signaling
+            convergence has occurred.
+        efreq (int): Iteration period in which information is printed to the user;
+            called frequency despite being inverse frequency."""
+    
+    if numgrad:
+        grad_routine = mol.define_gradient_routine_numerical()
+    else:
+        grad_routine = mol.define_gradient_routine_analytical()
+        
+    return descentDict[descent](mol, n, searchDict[search], mol.define_energy_routine(), grad_routine,
+                         efreq, nbnfreq, eprec*mol.ff.eunits, fprec*mol.ff.eunits/mol.ff.lunits)
+        
+def steepest_descent(mol, n, search, calc_e, calc_grad, efreq, nbn, eprec, fprec):
+    """Minimize the energy of the inputted molecule via the steepest descent approach."""
+    
+    #initial guess for stepsize
+    stepSize = 1e-1
+    
+    energy = calc_e()
+    gradient, maxForce, totalMag, = calc_grad()
+    eList = [energy]
+    print('energy:   %s' % energy)
+    print('maxforce: %s' % maxForce)
+    
+    for step in range(1, n+1):
+        
+        #calculate the stepsize
+        stepSize = search(mol, -gradient/totalMag, energy, gradient, calc_e, alpha=stepSize)
+        
+        #take the step
+        mol.posList += stepSize*(-gradient/totalMag)
+        
+        #reset nonbonded neighbors
+        if mol.ff.lj:
+            if step % nbn == 0:
+                mol._configure_nonbonded_neighbors()
+        
+        #reset quantities
+        energy = calc_e()
+        gradient, maxForce, totalMag = calc_grad()
+        
+        #for every multiple of efreq, print the status
+        if step % efreq == 0:
+            print('step:     %s' % step)
+            print('energy:   %s' % energy)
+            print('maxforce: %s' % maxForce)
+            eList.append(energy)
+            
+        #break the iteration if our forces are small enough
+        if maxForce < fprec:
+            print('###########\n Finished! \n###########')
+            print('step:     %s' % step)
+            print('energy:   %s' % energy)
+            print('maxforce: %s' % maxForce)
+            break
+    
+    return mol, eList
+    
+def conjugate_gradient(mol, n, search, calc_e, calc_grad, efreq, nbn, eprec, fprec):
+    """Minimize the energy of the inputted molecule via the conjugate gradient approach."""
+    
+    #initial guess for stepsize
+    stepSize = 1e-1
+    
+    #starting values
+    energy = calc_e()
+    gradient, maxForce, totalMag, = calc_grad()
+    eList = [energy]
+    print('energy:   %s' % energy)
+    print('maxforce: %s' % maxForce)
+    
+    gamma = 0.0
+    prevH = np.zeros([len(mol), 3])
+    
+    for step in range(1, n+1):
+        
+        #get the step direction
+        h = -gradient + gamma*prevH
+        normH = h/np.linalg.norm(np.hstack(h))
+        
+        #calculate the stepsize
+        stepSize = search(mol, normH, energy, gradient, calc_e, alpha=stepSize)
+        
+        #take the step
+        mol.posList += stepSize*(normH)
+        
+        #reset nonbonded neighbors
+        if mol.ff.lj:
+            if step % nbn == 0:
+                mol._configure_nonbonded_neighbors()
+        
+        #reset quantities
+        prevH = h
+        prevGrad = gradient
+        energy = calc_e()
+        gradient, maxForce, totalMag = calc_grad()
+        gamma = calculate_gamma(gradient, prevGrad)
+        
+        #for every multiple of efreq, print the status
+        if step % efreq == 0:
+            print('step:     %s' % step)
+            print('energy:   %s' % energy)
+            print('maxforce: %s' % maxForce)
+            eList.append(energy)
+            
+        #break the iteration if our forces are small enough
+        if maxForce < fprec:
+            print('###########\n Finished! \n###########')
+            print('step:     %s' % step)
+            print('energy:   %s' % energy)
+            print('maxforce: %s' % maxForce)
+            break
+    
+    return mol, eList
+    
+def parabolic_interpolation(a,b,c,fa,fb,fc):
+    
+    m = (b-a)*(fb-fc)
+    n = (b-c)*(fb-fa)
+    return b - ((b-c)*n - (b-a)*m)/(2.*copysign(max(abs(n-m), EP), n-m))
+
+def line_search_backtrack(mol, stepList, e, grad, calc_e, alpha=None):
+    """Return the stepsize determined by the backtracking strategies of
+    Armijo and Goldstein.
+    
+    Args:
+        mol (Molecule): Molecule to be minimized.  mol.posList is the coordinate array of the atoms
+        stepList (ndarray):  A N x 3 array of the step direction.
+        e (float): The energy of the molecule before the step is taken.
+        grad (ndarray): A N x 3 array of the forces on the atoms before the step is taken.
+        calc_e (function): Callable function that returns the energy of the molecule
+        alpha (float): An initial guess for the step size"""
+    
+    tau = 0.5
+    c = 0.33
+    alpha /= tau
+    
+    m = np.dot(np.hstack(stepList),np.hstack(grad))
+    if m > 0.:
+        raise ValueError("Step isn't a descent!")
+        
+    t = -c*m
+    
+    counter = 0
+    while alpha > EP:
+        
+        mol.posList += alpha*stepList
+        newE = calc_e()
+        mol.posList += -alpha*stepList
+        if e - newE >= alpha*t:
+#            print('counter:  %s' % counter)
+            return alpha
+        else:
+            alpha *= tau
+            counter += 1
+    
+#    print('counter:  %s' % counter)        
+    return EP
+    
+def line_search_brent(mol, stepList, e, grad, calc_e, alpha=None):
+    """Return the stepsize determined by Brent's method
+    
+    Args:
+        mol (Molecule): Molecule to be minimized.  mol.posList is the coordinate array of the atoms
+        stepList (ndarray):  A N x 3 array of the step direction.
+        e (float): The energy of the molecule before the step is taken.
+        grad (ndarray): A N x 3 array of the forces on the atoms before the step is taken.
+        calc_e (function): Callable function that returns the energy of the molecule"""
+        
+    a, b, c = bracket_minimum(mol, stepList, e, calc_e)
+        
+    return 0.
+    
+def bracket_minimum(mol, stepList, ea, calc_e):
+    """Return a tuple of 3 points a,b,c such that
+    f(a) > f(b) < f(c).  These points are stepsizes along the step direction
+    along the potential energy surface of the molecule."""
+    
+    #a will be the zero point
+    a = 0.
+    
+    #b will be some small distance away (we assume our step direction will minimize the energy within a finite range)
+    b = a + SQRTEP
+    mol.posList += b*stepList
+    eb = calc_e()
+    mol.posList += -b*stepList
+    
+    if eb > ea:
+        raise ValueError("There was an error in bracketing the minimum!")
+    
+    #c will be found through an iterative process
+    def ef(c):
+        mol.posList += c*stepList
+        e = calc_e()
+        mol.posList += -c*stepList
+        return e
+        
+    c = b + GR*(b-a)
+    ec = ef(c)
+    
+    while ec <= eb:
+        
+        x = parabolic_interpolation(a,b,c,ea,eb,ec)
+        xedge = b + 100.*(c-b)
+        
+        #if x is between b,c...
+        if ((b-x)*(x-c)) > 0.:
+            #check f(x)
+            ex = ef(x)
+            if ex < ec:
+                a,b = b,x
+                ea,eb = eb, ex
+                continue
+            elif ex > eb:
+                c,ec = x,ex
+                continue
+            x = c + GR*(c-b)
+            ex = ef(x)
+            
+        #if x is between c and our defined edge
+        elif ((c-x)*(x-xedge)) > 0.:
+            #check f(x)
+            ex=ef(x)
+            if ex < ec:
+                b,c,x = c,x,c+GR*(c-b)
+                eb,ec,ex = ec,ex,ef(x)
+        
+        #put x on the edge        
+        elif ((x-xedge)*(xedge-c)) > 0.:
+            x = xedge
+            ex = ef(x)
+         
+        #just slide along the search direction
+        else:
+            x = c + GR*(c-b)
+            ex = ef(x)
+        
+        a,b,c = b,c,x
+        ea,eb,ec = eb,ec,ex
+        
+    return a, b, c
+        
+descentDict = {"sd":steepest_descent, "cg":conjugate_gradient}
+searchDict = {"backtrack":line_search_backtrack, "brent":line_search_brent}
+
+def calculate_gamma(grad, pgrad):
+    """Return the 'gamma' factor in the conjugate gradient method
+    according to Fletcher and Reeves."""
+    grad = np.hstack(grad)
+    pgrad = np.hstack(pgrad)
+    return (np.dot(grad,grad))/(np.dot(pgrad,pgrad))
---
+++ krombopulos/nnet.py
@@ -0,0 +1,171 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class MLP:
+    """Multi-layer Perceptron of one hidden layer.  Hidden layer sigmoid threshold function.
+    
+    Arguments:
+        training (array-like): 2D array where rows correspond to the points.
+        target (array-like): 2D array of the target values, indexed like training data.
+        nh (int): Number of nodes in the hidden layer.
+        
+    Keywords:
+        beta (float): Parameter for the sigmoid threshold functions.
+        p (float): Momentum rate in training.
+        tbias (int): Number of bias nodes of -1 in the input layer. Default is 1.
+        hbias (int): Number of bias nodes of -1 in the hidden layer.  Default is 1.
+        outtype (str): Threshold function of the output node(s).  Choices are 'linear',
+            'logistic', or 'softmax'.  Default is 'linear'."""
+    
+    def __init__(self, training, target, nh, beta=1., p=.9, tbias=1, hbias=1, outtype="linear"):
+        self.training = np.concatenate((training, -np.ones((len(training), tbias))), axis=1)
+        self.target = np.array(target)
+        self.beta = beta
+        self.p = p
+        self.tbias = tbias
+        self.hbias = hbias
+        self.w1 = np.random.randn(self.training.shape[1], nh)/10.
+        self.w2 = np.random.randn(nh+self.hbias, self.target.shape[1])/10.
+        if outtype == 'linear':
+            def threshold(outputs):
+                return outputs
+            def deltao(outputs):
+                return (outputs-self.target)/self.target.shape[0]
+        elif outtype == 'logistic':
+            def threshold(outputs):
+                return 1./(1. + np.exp(-self.beta*outputs))
+            def deltao(outputs):
+                return self.beta*(outputs-self.target)*outputs*(1.-outputs)
+        self.threshold = threshold
+        self.deltao = deltao
+        self.outtype = outtype  #for train_seq
+            
+    def forward(self, inputs):
+        """Return the forward progress of the neural net."""
+        
+        #apply first set of weights
+        hidden = np.dot(inputs, self.w1)
+        #apply threshold function
+        hidden = 1./(1. + np.exp(-self.beta*hidden))
+        #prep the inputs to the output nodes
+        self.hidden = np.concatenate((hidden, -np.ones((inputs.shape[0], self.hbias))),axis=1)
+        
+        return self.threshold(np.dot(self.hidden, self.w2))
+        
+            
+    def train(self, lrate=.25, niter=100, print_err=True):
+        """Train the neural net."""
+        
+        #starting momentum
+        dw1 = np.zeros((self.w1.shape))
+        dw2 = np.zeros((self.w2.shape))
+        
+        for n in range(niter):
+            
+            outputs = self.forward(self.training)
+            
+            error = .5*np.sum((outputs-self.target)**2)
+            if n%100 == 0 and print_err:
+                print("Iteration: {0}".format(n))
+                print("Error: {0}".format(error))
+                
+            deltao = self.deltao(outputs)
+            deltah = self.hidden*self.beta*(1.-self.hidden)*(np.dot(deltao, np.transpose(self.w2)))
+            
+            dw1 = lrate*np.dot(np.transpose(self.training), deltah[:,:-1]) + self.p*dw1
+            dw2 = lrate*np.dot(np.transpose(self.hidden), deltao) + self.p*dw2
+            
+            self.w1 += -dw1
+            self.w2 += -dw2
+            
+    def train_seq(self, lrate=.25, niter=100):
+        """Train the neural net sequential (vs batch).
+        Doesn't fit rest of class, for Problem 4.5."""
+        
+        inputs = self.training
+        targets = self.target
+        counts = np.arange(self.training.shape[0])
+        change = np.copy(counts)
+        
+        for n in range(niter):
+            
+            if self.outtype == 'linear':
+                def deltao_seq(output, t):
+                    return output-t
+            elif self.outtype == 'logistic':
+                def deltao_seq(output, t):
+                    return self.beta(output-t)*output*(1.-output) 
+            
+            for count, point, target in zip(counts, inputs, targets):
+                
+                hidden = np.dot(point, self.w1)
+                hidden = 1./(1. + np.exp(-self.beta*hidden))
+                hidden = np.append(hidden, [-1]*self.hbias)
+                output = self.threshold(np.dot(hidden, self.w2))
+                deltao = deltao_seq(output, target)
+                deltah = hidden*self.beta*(1. - hidden)*(np.dot(deltao,np.transpose(self.w2)))
+                
+                self.w1 += -lrate*np.dot(np.transpose(point).reshape((len(np.transpose(point)), 1)), 
+                                         deltah[:-1].reshape((1,len(deltah)-1)))
+                self.w2 += -lrate*np.dot(np.transpose(hidden).reshape((len(np.transpose(hidden)),1)), 
+                                         deltao.reshape((1,len(deltao))))
+                
+            np.random.shuffle(change)
+            inputs = inputs[change,:]
+            targets = targets[change,:]
+                
+            
+    def estop(self, v, vtarget, lrate=.25, niter=100):
+        """Train the neural net but stop when the validation set's error stops to decrease."""
+        
+        v = np.concatenate((v, -np.ones((len(v), self.tbias))), axis=1)
+        vtarget = np.array(vtarget)
+        
+        #start with dummy errors
+        old_verr1 = 1e5 + 2.
+        old_verr2 = 1e5 + 1.
+        new_verr = 1e5
+        
+        count = 0
+        while ((old_verr1 - new_verr) > 1e-3 or (old_verr2 - old_verr1) > 1e-3):
+            self.train(lrate, niter, print_err=False)
+            old_verr2, old_verr1 = old_verr1, new_verr
+            vout = self.forward(v)
+            new_verr = .5*np.sum((vtarget-vout)**2)
+            count += 1
+            
+        print("Stopped")
+        print(new_verr)
+        print(old_verr1)
+        print(old_verr2)
+            
+#testing out       
+if __name__ == "__main__":
+    
+    x = np.ones((1,40))*np.linspace(0,1,40)
+    t = np.sin(2*np.pi*x) + np.cos(4*np.pi*x) + np.random.randn(40)*.2
+    x = x.T
+    t = t.T
+    
+    x = (x - x.mean(axis=0))/x.var(axis=0)
+    t = (t - t.mean(axis=0))/t.var(axis=0)
+    
+    plt.plot(x,t)
+    plt.show()
+    
+    train, traint = x[0::2,:], t[0::2,:]
+    test, testt = x[1::4,:], t[1::4,:]
+    valid, validt = x[3::4,:], t[3::4,:]
+    
+    print("Naive training")
+    net = MLP(train, traint, 3)
+#    net.train()
+    net.train_seq()
+
+    print("Early stopping")
+    net = MLP(train, traint, 3)
+    net.estop(valid, validt)       
+        
+        
